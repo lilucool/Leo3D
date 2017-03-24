1656c8e2a5937e20edbcb43936dede682013d350
lilucool <lilucool@163.com>
2017-03-24 17:01:44 +0800

    first commit

---
+++ Assets/onMouseDrag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onMouseDrag : MonoBehaviour {
+
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void OnMouseDrag() {
+		rend.material.color -= Color.white * Time.deltaTime;
+	}
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
